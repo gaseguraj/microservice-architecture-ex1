5d278c2eb4a02d28ccb529e79b9378e8d070e706
German Segura <german.andres.segura@gmail.com>
2021-10-29 23:10:57 -0400

    Both microservices registered in Eureka

--- nationality-predict/src/main/java/com/gsegura/microarch/nationalitypredict/NationalityPredictApplication.java
+++ nationality-predict/src/main/java/com/gsegura/microarch/nationalitypredict/NationalityPredictApplication.java
@@ -2,8 +2,10 @@ package com.gsegura.microarch.nationalitypredict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class NationalityPredictApplication {
 
     public static void main(String[] args) {
